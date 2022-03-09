806ed663a6f2b0bea82d00498dad98503d2c93a5
Priya Chandak <priya.chandak@vishleshak.io>
2022-03-09 16:49:37 +0530

    updated changes

--- demo.py
+++ demo.py
@@ -2,5 +2,9 @@ hungry = input("are you hungry?")
 
 if hungry=='yes':
     print('eat something')
+    print('eat Pizza')
+    print('eat burger')
+elif hungry=='no':
+    print('Ok')
 else:
     print("That's better")
